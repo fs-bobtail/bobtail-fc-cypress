8a579daad808a1e5d5ea6dd7964f3c026603fa18
Mehreen Hamraz <mehreenhamraz@Mehreens-MacBook-Pro-2.local>
2025-06-18 15:18:04 +0500

    add profile menu test

--- cypress/e2e/tests/dashboardTests.cy.js
+++ cypress/e2e/tests/dashboardTests.cy.js
@@ -8,11 +8,21 @@ describe("Dashboard Tests", function () {
     cy.fixture("testdata.json").then((creds) => {
       loginPage.login(creds.email, creds.password);
     });
+    dashboardPage.verifyAndClosePostLoginPopup();
   });
 
   it("TC1 - Verify header buttons on dashboard", function () {
-    dashboardPage.verifyAndClosePostLoginPopup();
     dashboardPage.verifyFleetButtonIsSelected();
     dashboardPage.verifyLoadsaAndFactoringButtonURL();
+  });
+
+  it("TC1 - Verify account menu shows correct name and email", function () {
+    cy.fixture("testdata.json").then((data) => {
+      dashboardPage.accountMenuBtn().contains(data.companyName);
+      dashboardPage.accountMenuBtn().click();
+      dashboardPage.accountMenuName().contains(data.companyName);
+      dashboardPage.accountMenuEmail().contains(data.email);
+      dashboardPage.logoutBtn().should('exist');
+    });
   });
 });
