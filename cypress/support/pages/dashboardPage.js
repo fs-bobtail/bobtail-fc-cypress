c1634d54c3bb42f79e2fbc610d9e823ca97ddc09
Mehreen Hamraz <mehreenhamraz@Mehreens-MacBook-Pro-2.local>
2025-07-24 17:31:21 +0500

    Complete Dashboard Tests

--- cypress/support/pages/dashboardPage.js
+++ cypress/support/pages/dashboardPage.js
@@ -8,12 +8,47 @@ class DashboardPage {
   factoringButton = () => cy.get('button[data-cy="products-menu-item-1"]');
   fleetButton = () => cy.get('button[data-cy="products-menu-item-2"]');
   accountMenuBtn = () => cy.get('div[data-cy="business-menu-trigger"]');
-  accountMenuName = () => cy.get('div[data-cy="business-menu-profile-section"] label').first();
-  accountMenuEmail = () => cy.get('div[data-cy="business-menu-profile-section"] label').last();
+  accountMenuName = () =>
+    cy.get('div[data-cy="business-menu-profile-section"] label').first();
+  accountMenuEmail = () =>
+    cy.get('div[data-cy="business-menu-profile-section"] label').last();
   logoutBtn = () => cy.get('div[data-cy="business-menu-item-0"]');
-  weeklyCreditLimit = () => cy.get('p[data-cy="cardsBreakDown-remainingLimit"]');
+  weeklyCreditLimit = () =>
+    cy.get('p[data-cy="cardsBreakDown-remainingLimit"]');
+  paymentHeading = () => cy.get('p[data-cy="upcoming-payment-heading"]');
+  statementsButton = () => cy.contains("button", "View Statements");
+  expenseHistorySection = () => cy.get('div[data-cy="card-expense-container"]');
+  transactionsTableHeader = () =>
+    cy.get('div[data-cy="recent-activity-table-headers"] p');
+  viewAllTransactions = () => cy.get('div[data-cy="recent-activity-viewAll"]');
+  selectedBankSection = () =>
+    cy.get('div[data-cy="upcoming-payment-selectedBank"]');
+  editBankSection = () =>
+    cy.get('div[data-cy="upcoming-payment-editBank"]');
+  addBankBtn = () => cy.get('.add-bank-btn');
+  existingAccountBox = () => cy.get('.account-box');
+  doneBtn = () => cy.get('button[data-cy="bankListing-doneBtn"]');
+  viewDepositsButton = () => cy.contains("button", "View Deposits");
+  goToSavingsButton = () => cy.contains("button", "Go to savings");
 
-  //methods
+  verifyTransactionTableColumns() {
+    const expectedHeaders = [
+      "Card",
+      "Truck",
+      "Driver",
+      "Date",
+      "Merchant",
+      "Gallons",
+      "Your Amount",
+      "Your Savings",
+      "Total Amount",
+    ];
+
+    this.transactionsTableHeader().each(($el, index) => {
+      cy.wrap($el).should("have.text", expectedHeaders[index]);
+    });
+  }
+
   verifyPostLoginUrlAndTitle() {
     cy.url({ timeout: 20000 }).should(
       "eq",
@@ -33,8 +68,21 @@ class DashboardPage {
     this.closeButton().click();
   }
 
+  verifyAndClosePostWeeklyCreditPopup() {
+    cy.contains("You've been approved for a weekly credit line of $2500");
+    this.closeButton().last().click();
+  }
+
   verifyFleetButtonIsSelected() {
     this.fleetButton().should("have.css", "background-color", "rgb(3, 89, 83)");
+  }
+
+  verifyPaymentsHeading() {
+    this.paymentHeading().contains("Payments");
+  }
+
+  clickStatementsBtn() {
+    this.statementsButton().click();
   }
 
   verifyLoadsaAndFactoringButtonURL() {
