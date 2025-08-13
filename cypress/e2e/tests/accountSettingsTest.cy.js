0abb4a9801b92243e0b5f6619bae90bd40ea872b
mubeen-aziz@live.com <mubeen-aziz@live.com>
2025-08-13 23:35:10 +0500

    Savings and Account Settings Test Cases

---
+++ cypress/e2e/tests/accountSettingsTest.cy.js
@@ -0,0 +1,28 @@
+import accountSettingsPage from "../../support/pages/accountSettingsPage";
+import dashboardPage from "../../support/pages/dashboardPage";
+import loginPage from "../../support/pages/loginPage";
+
+describe("Transaction Page Test Cases", function () {
+    before(function () {
+      cy.visit("/");
+      cy.fixture("testdata.json").then((creds) => {
+        loginPage.login(creds.testEmail, creds.testPassword);
+      });
+      dashboardPage.verifyAndClosePostLoginPopup();
+    });
+    
+  it("TC1 - Should navigate to Account Settings and verify title", function () {
+    accountSettingsPage.clickAccountSettingsOption();
+    accountSettingsPage.verifyAccountSettingsHeading();
+  });
+
+  it("TC2 - Should be able to navigate to Spend limits and verify spending limits", function () {
+    accountSettingsPage.clickSpendLimitsTab();
+  });
+
+  it("TC3 - Should be able to navigate to Notifications and Enable/Disable notifications", function () {
+    accountSettingsPage.clickNotificationsTab();
+    accountSettingsPage.clickNotificationsToggle();
+    accountSettingsPage.verifyNotificationsToast();
+  });
+});
---
+++ cypress/e2e/tests/savingsTest.cy.js
@@ -0,0 +1,18 @@
+import savingsPage from "../../support/pages/savingsPage";
+import dashboardPage from "../../support/pages/dashboardPage";
+import loginPage from "../../support/pages/loginPage";
+
+describe("Transaction Page Test Cases", function () {
+    before(function () {
+      cy.visit("/");
+      cy.fixture("testdata.json").then((creds) => {
+        loginPage.login(creds.testEmail, creds.testPassword);
+      });
+      dashboardPage.verifyAndClosePostLoginPopup();
+    });
+
+  it("TC1 - Should navigate to Savings page and verify title", function () {
+    savingsPage.clickSavingsOption();
+  });
+
+});
